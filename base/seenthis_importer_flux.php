f4dd6442283fc6e6f564a81a6a259ee56d49d5f8
Fil <fil@rezo.net>
2013-01-24 11:51:57 +0000

    plugin seenthis_importer_flux, version 1.0.0
    git-svn-id: svn://trac.rezo.net/seenthis/seenthis_importer_flux@355 726c7f6e-f4ed-4363-883b-90581dc1cf32

---
+++ base/seenthis_importer_flux.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * Seenthis_importer_flux : ajouter un champ 'rss' dans spip_auteurs
+ *
+ */
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function seenthis_importer_flux_declarer_tables_principales($tables_principales){
+	$tables_principales['spip_auteurs']['field']['rss'] = "varchar(256)";
+	return $tables_principales;
+}
+
+function seenthis_importer_flux_install($action,$prefix,$version_cible){
+	$version_base = $GLOBALS[$prefix."_base_version"];
+	switch ($action){
+		case 'test':
+			$ok = (isset($GLOBALS['meta'][$prefix."_base_version"])
+				AND version_compare($GLOBALS['meta'][$prefix."_base_version"],$version_cible,">="));
+			return $ok;
+			break;
+		case 'install':
+			maj_tables(array(
+				'spip_auteurs',
+			));
+			ecrire_meta($nom_meta_base_version,
+				$current_version=$version_cible, 'non');
+			break;
+		case 'uninstall':
+			sql_query("ALTER TABLE spip_auteurs DROP rss");
+			break;
+	}
+}
+
+
+?>
---
+++ genie/seenthis_importer_flux.php
@@ -0,0 +1,129 @@
+<?php
+
+function seenthis_importer_flux_taches_generales_cron($taches_generales){
+	$taches_generales['seenthis_importer_flux'] = 60;
+
+	return $taches_generales;
+}
+
+function genie_seenthis_importer_flux($t){
+	$s = sql_query("SELECT id_auteur,login,rss, RAND() AS hasard FROM spip_auteurs WHERE rss>'' ORDER BY hasard LIMIT 1");
+
+	if ($t = sql_fetch($s)) {
+		include_spip('inc/distant');
+		include_spip('inc/syndic');
+		if ($url = $t['rss']
+		AND preg_match(',^https?://,', $url)
+		AND $rss = recuperer_page($url)
+		AND $articles = analyser_backend($rss)
+		AND is_array($articles)
+		) {
+			$articles = array_slice($articles,0,5);
+			$check = array();
+			foreach ($articles as $article) {
+				$action = seenthis_importer_rss_article($article, $t['id_auteur']);
+				if ($action == 2) {
+					# creation d'un nouveau message : on sort
+					break;
+				}
+			}
+		} else {
+			spip_log("probleme avec le RSS '$url' de l'auteur $t[login]", 'flux');
+		}
+	}
+
+	return 1;
+}
+
+function seenthis_importer_rss_article($article, $moi) {
+	$url = $article['url'];
+
+	# si l'url pointe un message local, il faut fav
+	if (preg_match(',^https?://('
+	.preg_quote(_HOST).'/messages/(\d+)|'
+	.preg_quote(_SHORT_HOST).'/([a-f0-9]+)'
+	.'),',
+	$url, $r)) {
+		if ($r[3]) { # short
+			$id_me = base_convert($r[3],36,10);
+		} else {
+			$id_me = $r[2];
+		}
+		spip_log("$url local: $id_me", 'flux');
+	}
+	else {
+		# Règles du jeu :
+		# 1. si un lien est deja en base mais dans un message
+		# effacé nous appartenant, on ne fait rien : cela permet
+		# de supprimer a la main un message ajouté par le rss, sans
+		# qu'il ne reviennent bégayer...
+		# 2. si un lien existe et appartient à quelqu'un d'autre,
+		# on le partage, sauf si on a bloqué la personne
+		$s = sql_query('SELECT t.id_me,m.id_auteur
+		FROM spip_me_tags AS t
+		INNER JOIN spip_me AS m ON t.uuid=m.uuid'
+		.' WHERE tag='.sql_quote($article['url'])
+		# todo : auteurs que je bloque / ou que je follow
+		# .' AND m.id_auteur NOT IN (0)'					
+		);
+		if ($deja = sql_fetch($s)) {
+			# $deja = array (id_me => id_me, id_auteur => id_auteur)
+			$id_me = $deja['id_me'];
+		}
+	}
+
+	# si rien, on cree
+	if (!$id_me) {
+		include_spip('inc/uuid');
+		$uuid = UUID::getuuid($moi.$url);
+		$message = $article['titre']."\n".$url;
+		if (strlen($desc = $article['descriptif'])
+		OR strlen($desc = $article['content'])) {
+			$desc = couper(supprimer_tags($desc),500);
+			$desc = str_replace('&nbsp;', ' ', $desc);
+			$message .= "\n\n❝".$desc."❞";
+		}
+		if (is_array($article['tags'])) {
+			$tags = '';
+			foreach ($article['tags'] as $tag) {
+				$tag = importer_charset(supprimer_tags($tag));
+				$tag = str_replace(' ', '_', $tag);
+				if (preg_match('/'.preg_quote($tag).'/i', $message)) {
+					$message = preg_replace('/(^|[^#])('.preg_quote($tag).')/i',
+						'$1#$2', $message, 1);
+				} else {
+					$tags .= '#'.$tag.' ';
+				}
+			}
+			if ($tags)
+				$message .= "\n\n".trim($tags);
+		}
+		spip_log("creation $uuid $message",'flux');
+		instance_me($moi, $message,  $id_me=0, $id_parent=0, $id_dest=0, $ze_mot=0, $time="NOW()", $uuid);
+		return 2;
+	}
+
+	// on a trouvé un message :
+	// s'il est a nous, ou si on l'a deja partage, ne rien faire
+	if ($id_me) {
+		$mess = sql_allfetsel('*', 'spip_me', "id_me=$id_me");
+		if ($mess[0]['id_auteur'] == $moi) {
+			spip_log("$id_me deja envoye par $moi ($url)", 'flux');
+			return 0;
+		}
+
+		$share = sql_allfetsel('*', 'spip_me_share', "id_me=$id_me AND id_auteur=$moi");
+		if (count($share)) {
+			spip_log("$id_me deja partage par $moi ($url)", 'flux');
+			return 0;
+		}
+	}
+
+	// sinon, ajouter un partage
+	spip_log("$moi partage $id_me ($url)", 'flux');
+	sql_insertq('spip_me_share', array('id_me' => $id_me, 'id_auteur' => $moi, 'date' => 'NOW()'));
+	cache_me($id_me);
+	return 1;
+}
+
+?>
