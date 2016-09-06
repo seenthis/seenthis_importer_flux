17728c938333faf52e119305de05fcb2a71d5a3b
b_b <brunobergot@gmail.com>
2016-09-06 10:31:05 +0200

    log lors de la désactivation d'un flux

--- genie/seenthis_importer_flux.php
+++ genie/seenthis_importer_flux.php
@@ -20,6 +20,7 @@ function genie_seenthis_desactiver_flux($t){
 	$s = sql_query("SELECT id_auteur,login,en_ligne,email,rss FROM spip_auteurs WHERE rss > '' AND en_ligne < $mydate ORDER BY en_ligne DESC LIMIT 1");
 
 	if ($t = sql_fetch($s)) {
+		spip_log("desactivation du flux RSS de ". $t['login'] . " (". $t['id_auteur'] .") : ". $t['rss'], 'flux');
 		// ajouter une * au début de l'url du flux pour le désactiver
 		sql_updateq('spip_auteurs', array('rss' => '*' . $t['rss']),'id_auteur =' . $t['id_auteur'])
 		// envoyer un email à l'auteur pour le prévenir qu'on a désactivé son flux
