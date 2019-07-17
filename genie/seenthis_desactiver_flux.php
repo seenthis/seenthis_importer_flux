6282ddd01f879f47cc35714dd48706d2b883e194
brunob <bruno@eliaz.fr>
2019-07-17 14:30:17 +0200

    ne pas spécifier l'adresse d'expéditeur des notifications en dur
    ref seenthis/seenthis_squelettes#205

--- genie/seenthis_desactiver_flux.php
+++ genie/seenthis_desactiver_flux.php
@@ -20,8 +20,7 @@ function genie_seenthis_desactiver_flux($t){
 		// envoyer un email à l'auteur pour le prévenir qu'on a désactivé son flux
 		include_spip('inc/notifications');
 		$texte = recuperer_fond('notifications/flux_desactive', array('id_auteur'=>$t['id_auteur']));
-		$from = $GLOBALS['meta']['nom_site'] . " <no-reply@" . _HOST . ">";
-		notifications_envoyer_mails($t['email'], $texte, '', $from);
+		notifications_envoyer_mails($t['email'], $texte);
 	}
 
 	return 1;
