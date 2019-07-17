afa03cf303b7e70279178565f1e7fe82c0fd7d12
brunob <bruno@eliaz.fr>
2019-07-17 10:39:27 +0200

    Permettre de moduler les valeurs du nombre de flux traités dans le génie
    Fix seenthis/seenthis_squelettes#172

--- seenthis_importer_flux_pipelines.php
+++ seenthis_importer_flux_pipelines.php
@@ -5,11 +5,17 @@ if (!defined("_ECRIRE_INC_VERSION")) {
 }
 
 function seenthis_importer_flux_taches_generales_cron($taches_generales){
+	if (!defined('_SEENTHIS_IMPORTER_FLUX_NB_IMPORTER')) {
+		define('_SEENTHIS_IMPORTER_FLUX_NB_IMPORTER', 10);
+	}
+	if (!defined('_SEENTHIS_IMPORTER_FLUX_NB_DESACTIVER')) {
+		define('_SEENTHIS_IMPORTER_FLUX_NB_DESACTIVER', 10);
+	}
 	// duree du cron a moduler en fonction du nombre de flux...
 	// pour le moment, on en prend un au hasard à chaque tour
-	$taches_generales['seenthis_importer_flux'] = 10;
+	$taches_generales['seenthis_importer_flux'] = _SEENTHIS_IMPORTER_FLUX_NB_IMPORTER;
 	// désactiver le flux des auteurs qui ne se sont pas logués depuis 6 mois
-	$taches_generales['seenthis_desactiver_flux'] = 10;
+	$taches_generales['seenthis_desactiver_flux'] = _SEENTHIS_IMPORTER_FLUX_NB_DESACTIVER;
 
 	return $taches_generales;
 }
